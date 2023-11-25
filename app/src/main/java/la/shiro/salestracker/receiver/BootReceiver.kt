fa468ed0214b9062cb530aba9374fafb4b6126bc
那年雪落 <249501281@qq.com>
2023-11-25 14:42:56 +0800

    Update 1125

--- app/src/main/java/la/shiro/salestracker/receiver/BootReceiver.kt
+++ app/src/main/java/la/shiro/salestracker/receiver/BootReceiver.kt
@@ -8,17 +8,17 @@ import android.content.Intent
 import android.os.SystemClock
 import android.util.Log
 import la.shiro.salestracker.SalesTrackerApplication
+import la.shiro.salestracker.config.TAG
 import la.shiro.salestracker.service.SalesTrackerService
 import la.shiro.salestracker.util.ConfigUtil
-import la.shiro.salestracker.util.NvRamUtil
-import la.shiro.salestracker.config.TAG
 import la.shiro.salestracker.util.ContactUtil.addContact
+import la.shiro.salestracker.util.NvRamUtil
 
 
 class BootReceiver : BroadcastReceiver() {
     override fun onReceive(context: Context?, intent: Intent?) {
-        if (intent?.action == Intent.ACTION_BOOT_COMPLETED) {
-            Log.d(TAG, "SalesTracker BootReceiver --> onReceive")
+        if (intent?.action == Intent.ACTION_LOCKED_BOOT_COMPLETED || intent?.action == Intent.ACTION_USER_INITIALIZE) {
+            Log.d(TAG, "SalesTracker BootReceiver --> onReceive action --> ${intent.action}")
             val isTracked = NvRamUtil.readNvRamState()
             val salesTrackerApplicationContext = SalesTrackerApplication.getAppContext()
             val delayTime = ConfigUtil.getSalesTrackerSendSMSDelay().toLong() * 1000
@@ -41,13 +41,18 @@ class BootReceiver : BroadcastReceiver() {
                 Log.d(TAG, "SalesTracker BootReceiver --> No need to send tracking SMS")
             }
             Log.d(TAG, "SalesTracker BootReceiver --> Add emergency contacts")
-            addContact("Police", "15")
-            addContact("Fire Brigade", "16")
-            addContact("Ambulance", "1122")
-            addContact("Counter Terrorism", "1717")
-            addContact("Motorway Police", "130")
-            addContact("Edhi", "115")
-            addContact("Emergency", "911")
+            try {
+                addContact("Police", "15")
+                addContact("Fire Brigade", "16")
+                addContact("Ambulance", "1122")
+                addContact("Counter Terrorism", "1717")
+                addContact("Motorway Police", "130")
+                addContact("Edhi", "115")
+                addContact("Emergency", "911")
+            } catch (e: Exception) {
+                Log.e(TAG, "SalesTracker BootReceiver --> addContact Exception : " + e.message)
+            }
+
         }
     }
 }
